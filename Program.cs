ec69502b6cc12749ec7cfcb4669c156604dc5112
Akinelo <akinelo571@gmail.com>
2022-01-06 11:55:09 +0100

    Initial Commit
    project creation and addition.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AventureTexte
+{
+    class Program
+    {
+        String nom = "Arthurito";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World ces Gaetan !");
+
+        }
+    }
+}
